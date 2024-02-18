20aec62a71087c9a366dc774f3f6f054ce955270
eddiepeters00 <eddie.peters00@outlook.com>
2024-02-18 16:26:52 +0100

    Added content to projects session

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -4,14 +4,12 @@ import {
   js_svg,
   node_svg,
   react_svg,
-  ts_svg,
 } from "../../assets/images";
 import Card from "../../components/Card/Card";
 import { TechStack } from "../../components/TechStack/TechStack";
 
 const techStack = [
   { name: "Javascript", src: js_svg },
-  { name: "TypeScript", src: ts_svg },
   { name: "React.js", src: react_svg },
   { name: "Node.js", src: node_svg },
   { name: "CSS", src: css_svg },
@@ -22,29 +20,69 @@ function Projects() {
   return (
     <Card.Grid>
       <Card>
-        <Card.Title>Title</Card.Title>
+        <Card.Title>Spotify App</Card.Title>
+        <Card.Content>
+          <Card.Description>
+            Streamline your Spotify experience with this innovative app designed
+            to effortlessly filter through your playlists.
+          </Card.Description>
+          <Card.Description>
+            I created this app with Spotifys API to handle my very long spotify
+            playlists. I came up with the idea one day when i felt like only
+            listen to rock songs from one of my playlists that consists of
+            several different genres.
+          </Card.Description>
+          <Card.Description>
+            <TechStack
+              title="Technologies used in this project"
+              technologies={techStack}
+            />
+          </Card.Description>
+        </Card.Content>
       </Card>
+
       <Card>
-        <Card.Title>Title</Card.Title>
-        <Card.Description>
-          <TechStack
-            title="Technologies used in this project"
-            technologies={techStack}
-          />
-        </Card.Description>
+        <Card.Title>Spotify App</Card.Title>
+        <Card.Content>
+          <Card.Description>
+            Streamline your Spotify experience with this innovative app designed
+            to effortlessly filter through your playlists.
+          </Card.Description>
+          <Card.Description>
+            I created this app with Spotifys API to handle my very long spotify
+            playlists. I came up with the idea one day when i felt like only
+            listen to rock songs from one of my playlists that consists of
+            several different genres.
+          </Card.Description>
+          <Card.Description>
+            <TechStack
+              title="Technologies used in this project"
+              technologies={techStack}
+            />
+          </Card.Description>
+        </Card.Content>
       </Card>
 
-      <Card url="">
-        <Card.Title>Here is a card with a long title</Card.Title>
-        <Card.Description>
-          This is a description of one of my projects with a superlongword in
-          the very long description
-        </Card.Description>
-      </Card>
-
-      <Card url="">
-        <Card.Title>Here is a card</Card.Title>
-        <Card.Description>This is a description</Card.Description>
+      <Card>
+        <Card.Title>Spotify App</Card.Title>
+        <Card.Content>
+          <Card.Description>
+            Streamline your Spotify experience with this innovative app designed
+            to effortlessly filter through your playlists.
+          </Card.Description>
+          <Card.Description>
+            I created this app with Spotifys API to handle my very long spotify
+            playlists. I came up with the idea one day when i felt like only
+            listen to rock songs from one of my playlists that consists of
+            several different genres.
+          </Card.Description>
+          <Card.Description>
+            <TechStack
+              title="Technologies used in this project"
+              technologies={techStack}
+            />
+          </Card.Description>
+        </Card.Content>
       </Card>
     </Card.Grid>
   );
